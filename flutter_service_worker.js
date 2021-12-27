2efc44679df4a7c30c02be22fd88a10d2d985472
mahboob000 <mahboob.cs000@gmail.com>
2021-12-27 14:01:05 +0500

    Version 2.2.3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "version.json": "2b2ff1ba804c160a642aa82fac51cc2c",
 "index.html": "77dabdc662a21853a8db08941e4a1374",
 "/": "77dabdc662a21853a8db08941e4a1374",
-"main.dart.js": "4f6a726b52719cf4019d9158ed59b3a0",
+"main.dart.js": "a84d5a21a2c142f43bf120ee7571022b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
